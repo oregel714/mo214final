41703bfa9d4c103f55b6a9e4c6cbc4c6dfd76d4b
oregel714 <oregel991@gmail.com>
2026-08-26 14:44:44 -0700

    committing scratch and R folder, see #1

---
+++ R/moving_average.R
@@ -0,0 +1,48 @@
+# The input to this function should be a data frame containing stream chemistry data
+moving_average <- function(site_data) {
+  # Initialize a tibble to contain the results
+  result <- tibble(
+    window_start = seq(
+      site_data$Sample_Date[1], 
+      site_data$Sample_Date[nrow(site_data)],
+       by = "9 weeks"
+      ),
+      no3_ugl = NA,
+      nh4_ugl = NA,
+      k_mgl = NA,
+      mg_mgl = NA,
+      ca_mgl = NA
+    # Fill in the rest of the ions
+
+  )
+
+  # Fill in the iterator and sequence
+  for (i in 1:nrow(result)) {
+    # Create variables for the start and end of the current window
+    w1 <- result$window_start[i]
+    w2 <- result$window_start[i] + 63
+
+    # Create a logical vector, called "in_window", that says which samples are inside the window
+    # Hint: you'll compare sample dates to the start and end of the window
+    in_window <- site_data$Sample_Date >= w1 & site_data$Sample_Date < w2
+
+    # Use indexing to pull out the ion concentrations that fall inside the window
+    NO3_N_window <- site_data$`NO3-N`[in_window]
+    NH4_N_window <- site_data$`NH4-N`[in_window]
+    k_window <- site_data$K[in_window]
+    Mg_window <- site_data$Mg[in_window]
+    Ca_window <- site_data$Ca[in_window]
+
+    # The line above gets potassium in the window. Get the rest of the ions too
+    
+    # Calculate the mean of each ion concentration and fill in the result
+    result$no3_ugl[i] <- mean(NO3_N_window, na.rm = TRUE)
+    result$nh4_ugl[i] <- mean(NH4_N_window, na.rm = TRUE)
+    result$k_mgl[i] <- mean(k_window, na.rm = TRUE)
+    result$mg_mgl[i] <- mean(Mg_window, na.rm = TRUE)
+    result$ca_mgl[i] <- mean(Ca_window, na.rm = TRUE)
+  }
+  
+  # Return the result
+  return(result)
+}
--- scratch/day1-data-exploration.R
+++ scratch/day1-data-exploration.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+source("R/moving_average.R")
 # Reading in data files 
 bisley_1 <- read_csv("data/QuebradaCuenca1-Bisley.csv")
 bisley_2 <- read_csv("data/QuebradaCuenca2-Bisley.csv")
@@ -7,10 +7,8 @@ bisley_3 <- read_csv("data/QuebradaCuenca3-Bisley.csv")
 rio_mameyes <- read_csv("data/RioMameyesPuenteRoto.csv")
 
 # Stacking data frames to create one ultimate data frame 
-binded_rows <- bind_rows(bisley_1, bisley_2, bisley_3, rio_mameyes)
-
 # Filtering data frames by columns and specific years 
-filtered_binded <- binded_rows |> 
+filtered_bisley_1 <- bisley_1 |> 
   select(
     "Sample_ID", "Sample_Date", "NO3-N", "NH4-N", "K", "Mg", "Ca"
   ) |> 
@@ -18,7 +16,7 @@ filtered_binded <- binded_rows |>
     Sample_Date >= ymd("1988-01-01") & Sample_Date < ymd("1995-01-01")
   )
 
-q1_filtered <- filtered_binded |> 
+q1_filtered <- filtered_bisley_1 |> 
   filter(
     Sample_ID == "Q1"
   )
@@ -26,8 +24,8 @@ q1_filtered <- filtered_binded |>
 concentrations <- tibble(
   "Sample_ID" = NA,
   window_start = seq(
-    filtered_binded$Sample_Date[1],
-    filtered_binded$Sample_Date[nrow(filtered_binded)],
+    filtered_bisley_1$Sample_Date[1],
+    filtered_bisley_1$Sample_Date[nrow(filtered_bisley_1)],
     by = "9 weeks"
 
   ),
@@ -39,62 +37,7 @@ concentrations <- tibble(
     
 )
 
-for (i in 1:nrow(concentrations)){
-  w1 <- concentrations$window_start[i]
-  w2 <- concentrations$window_start[i] + 63
-  NO3_N_value <- q1_filtered$'NO3-N'[
-    q1_filtered$Sample_Date >= w1 & q1_filtered$Sample_Date < w2
-  ]
-  NH4_N_value <- q1_filtered$'NH4-N'[
-    q1_filtered$Sample_Date >= w1 & q1_filtered$Sample_Date < w2
-  ]
-  K_value <- q1_filtered$K[
-    q1_filtered$Sample_Date >= w1 & q1_filtered$Sample_Date < w2
-  ]
-  Mg_value <- q1_filtered$Mg[
-    q1_filtered$Sample_Date >= w1 & q1_filtered$Sample_Date < w2
-  ]
-  Ca_value <- q1_filtered$Ca[
-    q1_filtered$Sample_Date >= w1 & q1_filtered$Sample_Date < w2
-  ]
-
-  q1_filtered$'NO3-N'[i] <- mean(NO3_N_value, na.rm = TRUE)
-  q1_filtered$'NH4-N'[i] <- mean(NH4_N_value, na.rm = TRUE)
-  q1_filtered$K[i] <- mean(K_value, na.rm = TRUE)
-  q1_filtered$Mg[i] <- mean(Mg_value, na.rm = TRUE)
-  q1_filtered$Ca[i] <- mean(Ca_value, na.rm = TRUE)
-}
-
-
-
-for (i in 1:nrow(qs_smoothed)) {
-  # i is our iterator
-  # 1:nrow(qs_smoothed) is our sequence
-  # i will take on those values, one at a time
-
-  # What's the start of the window? Call it w1
-  w1 <- qs_smoothed$window_start[i]
-  # What's the end of the window? Call it w2
-  w2 <- qs_smoothed$window_start[i] + 9
-  # What potassium values are in that window?
-  k_mgl_value <- qs_data$k_mgl[
-    qs_data$sample_date >= w1 & qs_data$sample_date < w2
-  ]
-  mg_mgl_value <- qs_data$mg_mgl[
-    qs_data$sample_date >= w1 & qs_data$sample_date < w2
-  ]
-  # What's the mean
-  qs_smoothed$k_mgl[i] <- mean(k_mgl_value, na.rm = TRUE)
-  qs_smoothed$mg_mgl[i] <- mean(mg_mgl_value, na.rm = TRUE)
-  # How do you put it in the result?
-}
-
-qs_smoothed_longer <- qs_smoothed |> 
-  pivot_longer(
-    cols = c(k_mgl, mg_mgl),
-    names_to = "Nutrients",
-    values_to = "Concentration"
-  )
+moving_average(q1_filtered) 
 
 
 
